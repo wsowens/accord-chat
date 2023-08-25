5f810802723c9fe930d7f604efa659c0607e819b
wsowens <will@ovvens.com>
2023-08-24 21:09:41 -0400

    make_service.sh warns properly when accord-chat user does not exist

--- make_service.sh
+++ make_service.sh
@@ -4,7 +4,7 @@ echo "Copying accord-chat.service -> /etc/systemd/system/"
 
 sed -e "s|/path/to/accord-chat|$(pwd -P)|g" accord-chat.service | tee /etc/systemd/system/accord-chat.service
 
-id "accord-chat" &> /dev/null && useradd -m|| echo -e  "\e[33mWarning, user 'accord-chat' does not exist\e[0m" >&2
+id "accord-chat" &> /dev/null || echo -e  "\e[33mWarning, user 'accord-chat' does not exist\e[0m" >&2
 
 echo  -e "\e[32mNow run 'sudo systemctl daemon-reload && sudo systemctl start accord-chat'\e[0m"
  
